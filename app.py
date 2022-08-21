66b5109f1f5802902360ce22a37d8da7289181c7
Amit Patil <amit@iagilor-2.local>
2022-08-20 19:57:48 -0500

    Added hostnmae

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask
+import socket
 
 app = Flask(__name__)
 
 
+
 @app.route('/')
 def hello():
-    return 'Hello from Docker!'
+    myHostName = socket.gethostname()
+    return 'Hello from container {}'.format(myHostName)
